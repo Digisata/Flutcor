633d8478e4bb9af1259c67dfedc7b0c0d47b6733
Hanif Naufal <hnaufal123@gmail.com>
2020-07-02 12:48:04 +0700

    change launcher icon

--- lib/sharedpreferences/app_sharedpreference.dart
+++ lib/sharedpreferences/app_sharedpreference.dart
@@ -5,9 +5,7 @@ class AppSharedPreferences {
     final SharedPreferences _sharedPreference =
         await SharedPreferences.getInstance();
     String _photoUrl = _sharedPreference.getString('photoUrl');
-    assert(_photoUrl != null);
     String _username = _sharedPreference.getString('username');
-    assert(_username != null);
     if (_photoUrl != photoUrl)
       await _sharedPreference.setString('photoUrl', photoUrl);
     if (_username != username)
@@ -17,9 +15,7 @@ class AppSharedPreferences {
   Future<List<dynamic>> getLocalData() async {
     SharedPreferences _sharedPreference = await SharedPreferences.getInstance();
     String _photoUrl = _sharedPreference.getString('photoUrl');
-    assert(_photoUrl != null);
     String _username = _sharedPreference.getString('username');
-    assert(_username != null);
     return [_photoUrl, _username];
   }
 }
